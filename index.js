1f0ebee81d4152750c26e6c6c6d473e20290d3e0
sagacious-shell-7968 <tommysytc.tc@gmail.com>
2020-12-30 15:59:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,23 +4,123 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
+   each: function(collection, callback) {
+  const newCollection = (Array.isArray(collection)) ? collection.slice() : Object.values(collection)
+      
+for(let i=0; i<newCollection.length; i++)
+callback(newCollection[i]);
+return collection;
+
+      
+    },
+
+    map: function(collection, callback) {
+  const newCollection = (Array.isArray(collection)) ? collection.slice() : Object.values(collection)
+      
+for(let i=0; i<newCollection.length; i++)
+newCollection[i]=callback(newCollection[i]);
+
+return newCollection;
+
 
     },
 
-    map: function() {
+    reduce: function(collection, callback, acc) {
+const newCollection = (Array.isArray(collection)) ? collection.slice() : Object.values(collection)
+      if(acc){
+for(let i=0; i<newCollection.length; i++)
+acc=callback(acc, newCollection[i]);}
+
+else {acc=newCollection[0];
+for(let i=1; i<newCollection.length; i++)
+acc=callback(acc, newCollection[i]);}
+
+return acc;
 
     },
 
-    reduce: function() {
+    find: function(collection, predicate) {
+const newCollection = (Array.isArray(collection)) ? collection.slice() : Object.values(collection)
+for(let i=0; i<newCollection.length; i++)
+if(predicate(newCollection[i]))
+return newCollection[i];
 
+return undefined;
     },
+ filter: function(collection, predicate) {
+const newCollection = (Array.isArray(collection)) ? collection.slice() : Object.values(collection)
+let array=[];
+for(let i=0; i<newCollection.length; i++)
+if(predicate(newCollection[i]))
+ array.push(newCollection[i]);
 
-    functions: function() {
-
+return array;
     },
-
-
+    size: function(collection){
+      const newCollection = (Array.isArray(collection)) ? collection.slice() : Object.values(collection)
+      return newCollection.length;
+    },
+    first: function (array, n){
+      if(n){
+        let newArray=[];
+      for(let i=0; i<n; i++)
+      newArray.push(array[i]);
+      return newArray;}
+      return array[0];
+      
+      
+    },
+last: function (array, n){
+      if(n){
+        let newArray=[];
+      for(let i=array.length-n-1; i<array.length; i++)
+      newArray.push(array[i]);
+      return newArray;}
+      return array[array.length-1];
+      
+      
+    },
+    compact: function(array){
+      let copy=[];
+      for(i=0; i<array.length; i++)
+      if(array[i])
+      copy.push(array[i])
+      return copy;
+      
+    },
+    sortBy: function(array, callBack)
+{
+  let copy=array.slice();
+  return copy.sort(function(a,b)
+  { return callBack(a)-callBack(b); });
+  
+  
+},
+// 
+uniq: function(collection, sorted, callBack) {
+      if (sorted) {
+        const sort = [collection[0]]
+      for (let i = 1; i < collection.length; i++) {
+        if (sort[i-1] !== collection[i])
+          sort.push(collection[i])
+      }
+      return sort
+    
+      } else if (!iteratee) {
+        return Array.from(new Set(collection))
+      } else {
+        const modifiedVals = new Set()
+        const uniqVals = new Set()
+        for (let val of collection) {
+          const moddedVal = iteratee(val)
+          if (!modifiedVals.has(moddedVal)) {
+            modifiedVals.add(moddedVal)
+            uniqVals.add(val)
+          }
+        }
+        return Array.from(uniqVals)
+      }
+    },
   }
 })()
 
